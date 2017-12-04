75e8637e7a96fc013e9d247ae13e88259de61ddd
Agathe Loubert <agaloubert@hotmail.com>
2017-12-04 18:38:08 +0100

    seed en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Charity.create(name: 'SOS Animaux en detresse', immatriculation: 'X2467890987', address: '38 rue de la folie, 75098 Paris', phone_number: 02.45.67.89.00, email: 'sos@gmail.com', contact_name: 'Lea Dupont',
+  text: 'Venez nous soutenir pour sauver des animaux en détresse, ils vous en seront très reconnaissant, vous aimerons et resterons à vos côtés! Alors soyez sympa et venez nous voir, Pepito et ses copains vous attendent avec impatience!' , datetime: Date.Today )
+
+# 3 charities
+# 6 projets

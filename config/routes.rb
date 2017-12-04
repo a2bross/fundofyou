1019f01601aac9a845a6a48a6ec3e94855fee0c0
Nicolas Lizop <nicolas.lizop@gmail.com>
2017-12-04 17:27:28 +0100

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # adding all CRUD routes for projects in order for admins to be able to add associations more easily
+  resources :projects
+
+  # user_criteria routes
+  resources :user_criteria, only: [:edit, :update]
+
+  # donation routes
+  resources :donations, only: [:new, :create]
+
+  # subscription routes (all CRUD routes for admin purposes, as well as to allow users to cancel (== edit) their subscription)
+  resources :subscriptions
 end

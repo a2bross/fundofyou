fb830bc224e990424f7da9a75628505ceefee6b7
Nicolas Lizop <nicolas.lizop@gmail.com>
2017-12-04 18:17:08 +0100

    added controller skeleton

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,29 @@
+class ProjectsController < ApplicationController
+  def index
+    # list all projects
+  end
+
+  def new
+    # new project
+  end
+
+  def create
+    # create a project
+  end
+
+  def show
+    # show a project
+  end
+
+  def edit
+    # edit a project
+  end
+
+  def update
+    # update a project
+  end
+
+  def destroy
+    # destroy a project
+  end
+end

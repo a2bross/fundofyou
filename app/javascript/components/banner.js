93a6ac6f970d3c49a7469083010fce07e6a262da
Agathe Loubert <agaloubert@hotmail.com>
2017-12-08 09:35:39 +0100

    sans typed.js

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,14 +1,14 @@
-import Typed from 'typed.js';
+// import Typed from 'typed.js';
 
 
-// if (document.querySelector('#banner-typed-text')) {
-// }
+// // if (document.querySelector('#banner-typed-text')) {
+// // }
 
-function loadDynamicBannerText() {
-  new Typed('#banner-typed-text', {
-    strings: ["Le site de dons aux associations d'utilité publique"],
-    typeSpeed: 50,
-    loop: true
-  });
-};
-export { loadDynamicBannerText };
+// function loadDynamicBannerText() {
+//   new Typed('#banner-typed-text', {
+//     strings: ["Le site de dons aux associations d'utilité publique"],
+//     typeSpeed: 50,
+//     loop: true
+//   });
+// };
+// export { loadDynamicBannerText };

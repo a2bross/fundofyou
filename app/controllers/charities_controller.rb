94f8770b16a85d98b54f90e886b39d5dd12870ac
Zoe Cade <zoe.cade@gmail.com>
2017-12-08 11:34:41 +0100

    started forms

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -1,15 +1,29 @@
 class CharitiesController < ApplicationController
+  before_action :set_charity, only: [:update, :edit, :show, :destroy]
 
   def index
+    @charitiess = Charity.all
   end
 
   def new
+    @charity = Charity.new
   end
 
   def create
+    @charity = Charity.new(charity_params)
+    if @charity.save
+      redirect_to charity_path(@charity)
+    else
+      render :new
+    end
   end
 
   def update
+    if @charity.update(charity_params)
+      redirect_to charity_path(@charity)
+    else
+      render :edit
+    end
   end
 
   def edit
@@ -19,7 +33,17 @@ class CharitiesController < ApplicationController
   end
 
   def destroy
+    @charity.destroy
+    redirect_to charities_path
   end
 
+  private
 
+  def set_charity
+    @charity = Charity.find(params[:id])
+  end
+
+  def charity_params
+    params.require(:charity).permit(:name, :immatriculation, :address, :phone_number, :email, :contact_name, :description, :photo)
+  end
 end

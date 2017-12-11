3e0982d57df772fc3750b9d3d8d6596316eb2daf
Agathe Loubert <agaloubert@hotmail.com>
2017-12-11 09:54:59 +0100

    test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,15 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-<<<<<<< HEAD
+
     @project1 = Project.find(10)
     @project2 = Project.find(5)
     @project3 = Project.find(4)
     @project4 = Project.find(8)
     @project5 = Project.find(6)
     @project6 = Project.find(9)
-=======
->>>>>>> 474c0ee852658ea334a5196ba9934ccb07af3ecf
+
   end
 
   def test

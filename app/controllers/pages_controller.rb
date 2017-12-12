4019676ffcb1c7cabedb3b0e26c9ef02b3b59193
Zoe Cade <zoe.cade@gmail.com>
2017-12-12 12:59:15 +0100

    modif home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,13 +4,6 @@ class PagesController < ApplicationController
   def home
     @ongoing_projects = Project.where(status: 10).order(end_date: :desc).first(3)
     @completed_projects = Project.where("completion_rate >= 100").order(end_date: :desc).first(3)
-    @project1 = Project.find(10)
-    @project2 = Project.find(5)
-    @project3 = Project.find(4)
-    @project4 = Project.find(8)
-    @project5 = Project.find(6)
-    @project6 = Project.find(9)
-
   end
 
   def test
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,9 @@ import "../components/form";
 import "../components/payment-form";
 import "../components/result";
 import "../components/project-index";
+import { hideFlashAfter5sec } from '../components/flashes';
 
-
+hideFlashAfter5sec();
 // import { loadDynamicBannerText } from '../components/banner';
 // loadDynamicBannerText();
 

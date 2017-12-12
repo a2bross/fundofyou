a764fd9a341624a505f10ff30f3dceaa2945e9e8
Nicolas Lizop <nicolas.lizop@gmail.com>
2017-12-12 12:16:28 +0100

    forgot to save a couple files

---
+++ app/policies/charity_policy.rb
@@ -0,0 +1,27 @@
+class CharityPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+  def destroy?
+    true
+  end
+end

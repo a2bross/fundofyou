28638d8c5c88058cad8693c1308c970f87ca5779
Agathe Loubert <agaloubert@hotmail.com>
2017-12-12 16:30:45 +0100

    pour récup les trucs de Zoe

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,14 +1,11 @@
-// import Typed from 'typed.js';
+import Typed from 'typed.js';
 
+function loadDynamicBannerText() {
+  new Typed('#banner-typed-text', {
+    strings: ["l'écologie", "l'humanitaire", "l'action sociale", "le patrimoine", "l'éducation", "la recherche"],
+    typeSpeed: 90,
+    loop: true
+  });
+}
 
-// // if (document.querySelector('#banner-typed-text')) {
-// // }
-
-// function loadDynamicBannerText() {
-//   new Typed('#banner-typed-text', {
-//     strings: ["Le site de dons aux associations d'utilité publique"],
-//     typeSpeed: 50,
-//     loop: true
-//   });
-// };
-// export { loadDynamicBannerText };
+export { loadDynamicBannerText };

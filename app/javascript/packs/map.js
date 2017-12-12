258c5534f24aa0425554d05408ac38a8602887f1
Nick Lizop <nicolas.lizop@gmail.com>
2017-12-12 10:39:52 +0100

    removed debugger

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -11,7 +11,6 @@ if (document.getElementById("map")) {
       console.log('click sur la tab donation');
       if (mapElement) { // don't try to build a map if there's no div#map to inject in
         const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
-        debugger
         const markers = JSON.parse(mapElement.dataset.markers);
         map.addMarkers(markers);
         if (markers.length === 0) {

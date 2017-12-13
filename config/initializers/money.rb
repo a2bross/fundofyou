bfc34069f606aaffd4438a7cf5f6cea907c894e7
Agathe Loubert <agaloubert@hotmail.com>
2017-12-13 11:31:27 +0100

    number done

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -5,6 +5,7 @@ MoneyRails.configure do |config|
   # To set the default currency
   #
   config.default_currency = :eur
+  config.default_format = { :thousands_separator => " " }
 
   # Set default bank object
   #
